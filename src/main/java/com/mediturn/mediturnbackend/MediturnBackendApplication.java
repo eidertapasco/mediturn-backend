8266b2f96d51ec479045b0b2fdcf41d82972ab4a
Eider Tapasco <eidertapasco.dev@gmail.com>
2026-03-11 10:32:21 -0500

    initial commit: configuraciòn inicial del proyecto MediTurn

---
+++ src/main/java/com/mediturn/mediturnbackend/MediturnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mediturn.mediturnbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediturnBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediturnBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mediturn/mediturnbackend/MediturnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediturn.mediturnbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediturnBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
